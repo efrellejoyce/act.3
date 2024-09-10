db5ca36e87c8e9bef9deed77b60ba14b6d1c9dff
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-09-10 12:59:24 +0700

    completed numbers section

---
+++ numbers.js
@@ -0,0 +1,18 @@
+Number.js
+let firstFavNum = 27;
+let secondFavNum = 13;
+
+
+let additionResult = firstFavNum = secondFavNum;
+let subtractionResult = firstFavNum - secondFavNum;
+let multipicationResult = firstFavNum * secondFavNum;
+let divisionResult = firstFavNum / secondFavNum;
+let modulusResult = firstFavNum % secondFavNum;
+let exponentiationResult = firstFavNum ** secondFavNum;
+
+console.log('Addition: ', firstFavNum + secondFavNum, '=', additionResult);
+console.log('Subtraction: ', firstFavNum - secondFavNum, '=', subtractionResult);
+console.log('Multiplication: ', firstFavNum * secondFavNum, '=', multipicationResult);
+console.log('Division: ', firstFavNum / secondFavNum, '=', divisionResult);
+console.log('Modulus: ', firstFavNum % secondFavNum, '=', modulusResult);
+console.log('Exponentiation: ', firstFavNum ** secondFavNum, '=', exponentiationResult);
